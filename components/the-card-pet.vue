53b2756c482b5aad1620e68650e222a8233f0dec
will <wlymes@gmail.com>
2024-02-29 14:45:55 -0300

    feat: card pet

---
+++ components/the-card-pet.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { Dumbbell, Cat, Heart, PawPrint, CalendarDays, Info } from 'lucide-vue-next'
+
+</script>
+
+<template>
+  <div class="relative flex w-full max-w-80 items-center justify-between rounded-lg bg-california-300/10 p-2 shadow-lg">
+    <Info class=" absolute right-2 top-2 size-5 cursor-pointer text-california-500" />
+    <article class="flex w-full items-center justify-center gap-5">
+      <img src="https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg"
+        class="size-40 rounded-lg bg-california-500 object-cover shadow-lg">
+
+      <section class="flex w-full flex-col items-center justify-between gap-4">
+        <section class="flex w-full flex-col items-start justify-between gap-2">
+          <span class="flex items-center justify-center gap-2 text-gray-400">
+            <Cat class="size-5 text-california-500" />
+            <!-- <Dog class="size-5 text-california-500" /> -->
+            <span class="text-xs font-bold text-california-950">Jujuba</span>
+          </span>
+
+          <span class="flex items-center justify-center gap-2 text-gray-400">
+            <PawPrint class="size-5 text-california-500" />
+            <span class="text-xs font-bold text-california-950">Shiba hyfun Inu</span>
+          </span>
+        </section>
+
+        <section class="flex w-full flex-col items-start justify-center gap-2">
+          <span class="flex items-center justify-center gap-2 text-gray-400">
+            <Dumbbell class="size-5 text-california-500" />
+            <span class="w-16 text-start text-xs font-bold text-california-950">0kg - 2kg</span>
+          </span>
+
+          <span class="flex items-center justify-center gap-2 text-gray-400">
+            <CalendarDays class="size-5 text-california-500" />
+            <span class="w-16 text-start text-xs font-bold text-california-950">4m - 8m</span>
+          </span>
+
+          <span class="flex items-center justify-center gap-2 text-gray-400">
+            <heart class="size-5 text-california-500" />
+            <span class="w-16 text-start text-xs font-bold text-california-950">Macho</span>
+          </span>
+        </section>
+      </section>
+    </article>
+  </div>
+</template>
--- pages/schedule.vue
+++ pages/schedule.vue
@@ -17,27 +17,14 @@ import { Plus, ListFilter } from 'lucide-vue-next'
     </article>
 
     <article
-      class="flex size-full flex-col items-center justify-start gap-4 rounded-3xl bg-california-500/20 px-4 py-6 shadow-lg lg:w-3/4">
+      class="flex size-full flex-col items-center justify-start gap-4 rounded-3xl bg-california-500/20 px-4 py-6 shadow-lg lg:w-10/12">
       <UButton class="flex w-40 items-center justify-center gap-2 self-end px-4 py-2 transition-all">
         <ListFilter class="size-4" /> Filter
       </UButton>
 
-      <section class="size-full rounded-lg bg-california-50 p-2">
-        <!-- <div class="flex h-60 w-80 items-center justify-between rounded-lg bg-california-500/10 p-2 shadow">
-          <div class="flex flex-col gap-2">
-            <div class="size-40 rounded-lg bg-california-500" />
-            <div class="ml-5 flex flex-col">
-              <span class="text-gray-400">Nome: <span class="font-bold text-california-950">Jujuba</span></span>
-              <span class="text-gray-400">Raça: <span class="font-bold text-california-950">Shiba Inu</span></span>
-            </div>
-          </div>
-
-          <div class="flex flex-col">
-            <span class="text-gray-400">Weight: <span class="font-bold text-california-950">20kg</span></span>
-            <span class="text-gray-400">Genre: <span class="font-bold text-california-950">Famale</span></span>
-            <span class="text-gray-400">Age: <span class="font-bold text-california-950">2 - 4</span></span>
-          </div>
-        </div> -->
+      <section
+        class="flex size-full flex-wrap items-start justify-start gap-4 overflow-y-auto rounded-lg bg-white p-2 scrollbar-hide">
+        <TheCardPet v-for="n in 22" :key="n" />
       </section>
     </article>
   </section>
