16008f5be8ff55b0239a35e843ab7f139431226e
will <wlymes@gmail.com>
2024-02-29 16:06:05 -0300

    feat: date picker calendary

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -4,22 +4,23 @@ import { Plus, ListFilter } from 'lucide-vue-next'
 
 <template>
   <section
-    class="flex min-h-screen w-full flex-col items-center justify-start gap-5 overflow-y-auto p-4 pb-20 scrollbar-hide lg:flex-row lg:justify-center lg:pb-5">
+    class="flex min-h-screen w-full flex-col items-center justify-start gap-5 overflow-y-auto p-4 pb-20 scrollbar-hide lg:flex-row lg:justify-center lg:pb-5 dark:bg-california-500/20">
     <article
-      class="flex size-full flex-col items-center justify-start gap-4 rounded-3xl bg-california-500/20 px-4 py-6 shadow-lg lg:w-3/12">
+      class="flex size-full flex-col items-center justify-between gap-4 rounded-3xl bg-california-500/20 px-4 py-6 shadow-lg lg:w-3/12">
       <UButton class="flex w-full items-center justify-center gap-2 self-end px-4 py-2 transition-all">
-        <Plus class="size-4" /> New Schedule
+        <Plus class="size-4" /> Novo Agendamento
       </UButton>
-      <div class="h-80 w-full rounded-lg bg-white shadow" />
+      <TheCalendary />
+
       <div class="h-20 w-full rounded-lg bg-white shadow" />
       <div class="h-20 w-full rounded-lg bg-white shadow" />
       <div class="h-60 w-full rounded-lg bg-white shadow" />
     </article>
 
     <article
-      class="flex size-full flex-col items-center justify-start gap-4 rounded-3xl bg-california-500/20 px-4 py-6 shadow-lg lg:w-10/12">
-      <UButton class="flex w-40 items-center justify-center gap-2 self-end px-4 py-2 transition-all">
-        <ListFilter class="size-4" /> Filter
+      class="flex size-full flex-col items-center justify-between gap-4 rounded-3xl bg-california-500/20 px-4 py-6 shadow-lg lg:w-10/12">
+      <UButton class="flex items-center justify-center gap-2 self-end px-4 py-2 transition-all">
+        <ListFilter class="size-4" /> Filtrar
       </UButton>
 
       <section
