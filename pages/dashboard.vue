2cdb016dbb9501b97cee70e102c3b1a905ce673c
will <wlymes@gmail.com>
2024-03-08 16:17:29 -0300

    feat: modal create form

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -28,11 +28,31 @@ const cardData = [
   }
 ]
 
+const { isOpen } = useToggle()
 </script>
 
 <template>
   <section
     class="flex min-h-screen w-full flex-col items-center justify-between gap-10 space-y-4 overflow-y-auto bg-[#f5f5f5] p-4 scrollbar-hide lg:flex-row lg:justify-center lg:space-y-0">
+    <UModal v-model="isOpen">
+      <UCard :ui="{ divide: 'divide-y divide-gray-100' }" class="bg-[#f5f5f5]">
+        <template #header>
+          <div class="flex items-center justify-between">
+            <h3 class="text-base font-semibold leading-6 text-california-950">
+              Agendamento
+            </h3>
+            <UButton icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="isOpen = !isOpen" />
+          </div>
+        </template>
+
+        <section class="grid w-full place-items-center space-y-14 rounded-lg p-4">
+          <the-preview-pet-card />
+          <div class="w-96 rounded-lg bg-white p-4 shadow-lg">
+            <the-modal-create-form />
+          </div>
+        </section>
+      </UCard>
+    </UModal>
     <article class="flex flex-col items-center justify-start gap-10 rounded-lg px-4 lg:h-full lg:max-w-96">
       <section class="flex flex-wrap items-center justify-evenly gap-8">
         <the-dash-card v-for="card in cardData" :key="card.title" :title="card.title" :subtitle="card.subtitle"
@@ -42,7 +62,7 @@ const cardData = [
           </template>
         </the-dash-card>
       </section>
-      <the-schedule-btn />
+      <the-schedule-btn @click="isOpen = !isOpen" />
     </article>
 
     <article class="flex size-full max-w-7xl flex-col items-center justify-center p-1">
--- pages/schedule.vue
+++ pages/schedule.vue
@@ -15,11 +15,33 @@ const cardData = [
     icon: Stethoscope
   }
 ]
+
+const { isOpen } = useToggle()
 </script>
 
 <template>
   <section
     class="flex min-h-screen w-full flex-col items-center justify-between gap-10 overflow-y-auto bg-[#f5f5f5] p-4 scrollbar-hide lg:flex-row lg:justify-center lg:pb-5">
+    <UModal v-model="isOpen">
+      <UCard :ui="{ divide: 'divide-y divide-gray-100' }" class="bg-[#f5f5f5]">
+        <template #header>
+          <div class="flex items-center justify-between">
+            <h3 class="text-base font-semibold leading-6 text-california-950">
+              Agendamento
+            </h3>
+            <UButton icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="isOpen = !isOpen" />
+          </div>
+        </template>
+
+        <section class="grid w-full place-items-center space-y-14 rounded-lg p-4">
+          <the-preview-pet-card />
+          <div class="w-96 rounded-lg bg-white p-4 shadow-lg">
+            <the-modal-create-form />
+          </div>
+        </section>
+      </UCard>
+    </UModal>
+
     <article
       class="flex size-full min-h-screen flex-col items-center justify-between space-y-8 overflow-y-auto scrollbar-hide md:min-h-max lg:max-w-96 lg:px-4">
 
@@ -31,7 +53,7 @@ const cardData = [
 
       <v-container>
         <v-row justify="center">
-          <the-schedule-btn />
+          <the-schedule-btn @click="isOpen = !isOpen" />
         </v-row>
       </v-container>
 
