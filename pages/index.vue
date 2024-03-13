ecad644fa9b0a3c852dcd573e4e3592ce7596215
will <wlymes@gmail.com>
2024-03-13 11:29:34 -0300

    feat: loading page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
+import { PawPrint } from 'lucide-vue-next'
+
+definePageMeta({
+  layout: false,
+  middleware: 'auth'
+})
+
+onMounted(() => {
+  setTimeout(() => {
+    useRouter().push('/dashboard')
+  }, 2000)
+})
 
 </script>
 
 <template>
-  <div class="flex min-h-screen w-full items-center justify-center bg-white">
-    <h2>loading</h2>
-  </div>
+  <article class="flex h-screen items-center justify-center">
+    <div class="container flex flex-col items-center justify-center gap-2">
+      <PawPrint class="animate-bounce text-primary" />
+      <span class="text-muted-foreground">CutePet</span>
+    </div>
+  </article>
 </template>
 
 <style scoped></style>
