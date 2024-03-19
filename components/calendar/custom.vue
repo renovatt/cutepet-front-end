876302581a0207819e2a710a8e85ff158984b7ba
will <wlymes@gmail.com>
2024-03-19 13:32:21 -0300

    feat: synchronizing data in custom calendar

--- components/calendar/custom.vue
+++ components/calendar/custom.vue
@@ -1,94 +1,41 @@
 <script setup lang="ts">
-const month = new Date().getMonth()
-const year = new Date().getFullYear()
+type ScheduleDataProps = {
+  key: string
+  customData: {
+    title: string
+    class: string
+  }
+  dates: Date
+}
+const { schedules } = useSchedules()
 
-const masks = ref({
-  weekdays: 'WWW',
+const masks = ref({ weekdays: 'WWW', })
+
+const scheduleData = ref<ScheduleDataProps[]>([])
+
+const attributes = computed(() => scheduleData.value)
+
+watch(schedules, (newSchedules) => {
+  scheduleData.value = newSchedules?.map(schedule => ({
+    key: schedule.id,
+    customData: {
+      title: schedule.service === 'clinic' ? 'Clínica' : ' Banho e tosa',
+      class: schedule.service === 'clinic' ? 'bg-blue-500 text-white' : 'bg-primary text-white',
+    },
+    dates: new Date(schedule.date),
+  })) || []
 })
 
-const attributes = ref([
-  {
-    key: 1,
-    customData: {
-      title: 'Lunch with mom.',
-      class: 'bg-red-600 text-white',
-    },
-    dates: new Date(year, month, 1),
-  },
-  {
-    key: 2,
-    customData: {
-      title: 'Take Noah to basketball practice',
-      class: 'bg-blue-500 text-white',
-    },
-    dates: new Date(year, month, 2),
-  },
-  {
-    key: 3,
-    customData: {
-      title: "Noah's basketball game.",
-      class: 'bg-blue-500 text-white',
-    },
-    dates: new Date(year, month, 5),
-  },
-  {
-    key: 4,
-    customData: {
-      title: 'Take car to the shop',
-      class: 'bg-indigo-500 text-white',
-    },
-    dates: new Date(year, month, 5),
-  },
-  {
-    key: 4,
-    customData: {
-      title: 'Meeting with new client.',
-      class: 'bg-teal-500 text-white',
-    },
-    dates: new Date(year, month, 7),
-  },
-  {
-    key: 5,
-    customData: {
-      title: "Mia's gymnastics practice.",
-      class: 'bg-pink-500 text-white',
-    },
-    dates: new Date(year, month, 11),
-  },
-  {
-    key: 6,
-    customData: {
-      title: 'Cookout with friends.',
-      class: 'bg-orange-500 text-white',
-    },
-    dates: { months: 5, ordinalWeekdays: { 2: 1 } },
-  },
-  {
-    key: 7,
-    customData: {
-      title: "Mia's gymnastics recital.",
-      class: 'bg-pink-500 text-white',
-    },
-    dates: new Date(year, month, 22),
-  },
-  {
-    key: 8,
-    customData: {
-      title: 'Visit great grandma.',
-      class: 'bg-red-600 text-white',
-    },
-    dates: new Date(year, month, 25),
-  },
-])
 </script>
 
 <template>
   <div class="text-center">
-    <Calendar class="custom-calendar max-w-full" :masks="masks" :attributes="attributes" disable-page-swipe is-expanded>
+    <Calendar class="custom-calendar max-w-full" :masks="masks" :attributes="attributes" disable-page-swipe
+      is-expanded>
       <template #day-content="{ day, attributes }">
         <div class="z-10 flex h-full flex-col overflow-hidden">
           <span class="day-label text-sm text-gray-900">{{ day.day }}</span>
-          <div class="grow overflow-auto scrollbar-hide">
+          <div class="relative grow overflow-auto scrollbar-hide">
             <p v-for="attr in attributes" :key="attr.key" class="mb-1 mt-0 rounded-sm p-1 text-xs leading-tight"
               :class="attr.customData.class">
               {{ attr.customData.title }}
